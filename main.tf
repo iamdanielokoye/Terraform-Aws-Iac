2ed542805ef047d31ee1323b2eea671d7f670128
iamdanielokoye <okoyedann@gmail.com>
2025-02-04 19:51:41 +0100

    Refactor Terraform configuration to use variable inputs for VPC, EC2, RDS, and S3 modules

--- main.tf
+++ main.tf
@@ -1,80 +1,37 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
+  source = "./modules/vpc"
 
-  name            = "secure-vpc"
-  cidr            = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  public_subnets  = ["10.0.1.0/24"]
-  private_subnets = ["10.0.2.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  
-  tags = {
-    Environment = "dev"
-    Project     = "IaC Security"
-  }
+  vpc_cidr_block = var.vpc_cidr_block
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  azs            = var.azs
 }
 
 module "ec2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.0.0"
+  source = "./modules/ec2"
 
-  name          = "secure-instance"
-  instance_type = "t3.micro"
-  ami          = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (example)
-  subnet_id    = module.vpc.public_subnets[0]
-  key_name     = "my-key"
-  
-  security_groups = [module.security_group.security_group_id]
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
-
-  name        = "secure-sg"
-  description = "Allow only SSH and HTTP"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_rules = ["ssh-tcp", "http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules = ["all-all"]
-}
-
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.2.0"
-
-  bucket = "secure-iac-bucket"
-  acl    = "private"
-  versioning = {
-    enabled = true
-  }
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  subnet_id     = module.vpc.private_subnets[0]
+  security_group_ids = [module.vpc.default_security_group_id]
 }
 
 module "rds" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "5.0.0"
+  source = "./modules/rds"
 
-  identifier = "secure-db"
-  engine     = "mysql"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-  username   = "admin"
-  password   = "securepassword123"
-  publicly_accessible = false
-  subnet_ids = module.vpc.private_subnets
-  vpc_security_group_ids = [module.security_group.security_group_id]
-  
-  tags = {
-    Environment = "dev"
-  }
+  db_name     = var.rds_db_name
+  db_username = var.rds_db_username
+  db_password = var.rds_db_password
+  subnet_ids  = module.vpc.private_subnets
+  vpc_id      = module.vpc.vpc_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = var.s3_bucket_name
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,54 @@
-variable "aws-region" {
-    description = "Aws region"
-    type = string
-    default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones"
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ec2_ami" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0c02fb55956c7d316"
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "rds_db_name" {
+  description = "Name of the RDS database"
+  default     = "mydatabase"
+}
+
+variable "rds_db_username" {
+  description = "Username for the RDS database"
+  default     = "admin"
+}
+
+variable "rds_db_password" {
+  description = "Password for the RDS database"
+  sensitive   = true
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  default     = "my-secure-bucket"
 }
