f74ab7411fe00a84960c83bd45104fa11141e640
iamdanielokoye <okoyedann@gmail.com>
2025-02-04 19:52:15 +0100

    Add descriptions to output variables for VPC, EC2, RDS, and S3 resources

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = module.ec2.instance_id
 }
 
 output "rds_endpoint" {
-  value = module.rds.endpoint
+  description = "Endpoint of the RDS instance"
+  value       = module.rds.rds_endpoint
 }
 
-output "public_subnets" {
-  value = module.vpc.public_subnets
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = module.s3.bucket_name
 }
