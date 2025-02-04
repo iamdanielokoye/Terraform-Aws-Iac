22c7b55f64135744bfff8db5410d0e2e5476b61c
iamdanielokoye <okoyedann@gmail.com>
2025-02-04 21:30:21 +0100

    Rename S3 bucket variable for clarity and consistency

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+  bucket = var.s3_bucket
 
   versioning {
     enabled = true
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,4 +1,4 @@
-variable "bucket_name" {
+variable "s3_bucket" {
   description = "Name of the S3 bucket"
   type        = string
 }
